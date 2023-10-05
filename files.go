de9faec30b301641965fee2be555fa3f0b5bca22
Anton Tayanovskyy <anton@pulumi.com>
2023-10-05 15:03:12 -0400

    Implement VerifyUgprade functionality

---
+++ files.go
@@ -0,0 +1,75 @@
+// Copyright 2016-2023, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package providertest
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func dirExists(t *testing.T, dir string) bool {
+	_, err := os.Stat(dir)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return false
+		}
+		require.NoError(t, err)
+	}
+	return true
+}
+
+func deleteFileIfExists(t *testing.T, file string) {
+	err := os.Remove(file)
+	if errors.Is(err, fs.ErrNotExist) {
+		return
+	}
+	require.NoError(t, err)
+}
+
+func writeFile(t *testing.T, file string, data []byte) {
+	ensureFolderExists(t, filepath.Dir(file))
+	err := os.WriteFile(file, data, 0755)
+	require.NoError(t, err)
+}
+
+func readFile(t *testing.T, file string) string {
+	bytes, err := os.ReadFile(file)
+	require.NoError(t, err)
+	return string(bytes)
+}
+
+func ensureFolderExists(t *testing.T, dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		require.NoError(t, err)
+	}
+}
+
+func findFiles(t *testing.T, dir string, matches func(string) bool) []string {
+	var files []string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if matches(path) {
+			files = append(files, path)
+		}
+		return nil
+	})
+	require.NoError(t, err)
+	return files
+}
