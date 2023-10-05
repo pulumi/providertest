b8848d6169a092a825f1748e14b1c4318d848a4c
Daniel Bradley <daniel@pulumi.com>
2023-10-05 17:21:42 +0100

    Stub out initial interface concept
    Use options pattern to keep the interface stable but easy to add to.
    - Separate configuration phase from test running.
    - Add support for starting the provider locally and attaching.
    - Add support for edit dirs as this is fairly core to what we use regularly. Default to "additive" files by having to opt in to "clean" removed files.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # providertest
+
 Incubating facilities for testing Pulumi providers
+
+## Example Usage
+
+```go
+NewProviderTest("test/dir",
+  WithProvider(StartLocalProvider),
+  WithEditDir("../dir2"))
+```
--- providertest.go
+++ providertest.go
@@ -13,3 +13,78 @@
 // limitations under the License.
 
 package providertest
+
+import (
+	"context"
+	"testing"
+)
+
+type ProviderTest struct {
+	dir              string
+	providerStartups []StartProvider
+	editDirs         []EditDir
+}
+
+// NewProviderTest creates a new provider test with the initial directory to be tested.
+func NewProviderTest(dir string, opts ...Option) *ProviderTest {
+	pt := &ProviderTest{
+		dir: dir,
+	}
+	for _, opt := range opts {
+		opt(pt)
+	}
+	return pt
+}
+
+type Option func(*ProviderTest)
+
+func (pt *ProviderTest) Configure(opts ...Option) {
+	for _, opt := range opts {
+		opt(pt)
+	}
+}
+
+// WithEditDir adds a step to the test which will overwrite the contents of the directory then execute an update.
+// If this is a relative path, it will be resolved relative to the original test directory.
+func WithEditDir(dir string, opts ...EditDir) Option {
+	return func(pt *ProviderTest) {
+		pt.editDirs = append(pt.editDirs, EditDir{dir: dir})
+	}
+}
+
+type EditDir struct {
+	dir   string
+	clean bool
+}
+
+type EditDirOption func(*EditDir)
+
+// WithEditDirClean will remove files from the directory which are not in the edit directory but were in the original directory.
+func WithEditDirClean() EditDirOption {
+	return func(ed *EditDir) {
+		ed.clean = true
+	}
+}
+
+// StartProvider is a function that starts a provider and returns the name and port it is listening on.
+// When the test is complete, the context will be cancelled and the provider should exit.
+type StartProvider func(ctx context.Context) (*ProviderAttach, error)
+
+type ProviderAttach struct {
+	// Name of the provider e.g. "aws"
+	Name string
+	// Port the provider is listening on
+	Port int
+}
+
+// WithProvider adds a provider to be started and attached for the test run.
+func WithProvider(start StartProvider) Option {
+	return func(pt *ProviderTest) {
+		pt.providerStartups = append(pt.providerStartups, start)
+	}
+}
+
+// Run starts executing the configured tests
+func (pt *ProviderTest) Run(t *testing.T) {
+	// TODO: Implement against program test
+}
