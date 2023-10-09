3080f026e57b2ef48710fba6e2d15605f324e62a
Daniel Bradley <daniel@pulumi.com>
2023-10-09 15:34:56 +0100

    Fix up SDK testing for first passed test

--- sdk.go
+++ sdk.go
@@ -2,6 +2,7 @@ package providertest
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 	"os/exec"
 	"testing"
@@ -18,15 +19,13 @@ func (pt *ProviderTest) RunSdk(t *testing.T, language string) {
 		return
 	}
 	ctx := context.Background()
-	stackName := fmt.Sprintf("test-%s", language)
-	stack, err := auto.NewStackLocalSource(ctx, stackName, convertedProgram.Dir)
+	stack := convertedProgram.NewStack(t, ctx)
+	err = stack.SetAllConfig(ctx, pt.GetConfig())
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	t.Cleanup(func() {
-		stack.Destroy(ctx)
-	})
+	convertedProgram.RestorePackages(t, ctx)
 
 	_, err = stack.Preview(ctx)
 	if err != nil {
@@ -35,7 +34,84 @@ func (pt *ProviderTest) RunSdk(t *testing.T, language string) {
 	}
 }
 
+func (pt *ProviderTest) GetConfig() auto.ConfigMap {
+	config := auto.ConfigMap{}
+	for k, v := range pt.config {
+		config[k] = auto.ConfigValue{Value: v}
+	}
+	return config
+}
+
+func (convertedProgram *ConvertedProgram) NewStack(t *testing.T, ctx context.Context) *auto.Stack {
+	t.Helper()
+
+	rand := RandomString()
+	stackName := fmt.Sprintf("%s-%s", convertedProgram.Language, rand)
+	stack, err := auto.NewStackLocalSource(ctx, stackName, convertedProgram.Dir)
+
+	if convertedProgram.Language == "python" {
+		workspace := stack.Workspace()
+		settings, err := workspace.ProjectSettings(ctx)
+		if err != nil {
+			t.Fatalf("failed loading project settings: %v", err)
+		}
+		settings.Runtime.SetOption("virtualenv", "venv")
+		err = workspace.SaveProjectSettings(ctx, settings)
+		if err != nil {
+			t.Fatalf("failed saving project settings: %v", err)
+		}
+	}
+	if err != nil {
+		t.Error(err)
+		return nil
+	}
+	t.Cleanup(func() {
+		stack.Destroy(ctx)
+		stack.Workspace().RemoveStack(ctx, stackName)
+	})
+	return &stack
+}
+
+func RandomString() string {
+	b := make([]byte, 8)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+func (convertedProgram *ConvertedProgram) RestorePackages(t *testing.T, ctx context.Context) {
+	t.Helper()
+
+	t.Logf("restoring packages for %s", convertedProgram.Language)
+	var commands []*exec.Cmd
+	switch convertedProgram.Language {
+	case "csharp":
+		commands = append(commands, exec.Command("dotnet", "restore"))
+	case "go":
+		commands = append(commands, exec.Command("go", "mod", "download"))
+	case "python":
+		commands = append(commands,
+			exec.Command("python3", "-m", "venv", "venv"),
+			exec.Command("venv/bin/pip", "install", "-r", "requirements.txt"))
+	case "typescript":
+		commands = append(commands, exec.Command("npm", "install"))
+	default:
+		t.Errorf("unknown language %s", convertedProgram.Language)
+		return
+	}
+	for _, cmd := range commands {
+		t.Logf("running %s", cmd.String())
+		cmd.Dir = convertedProgram.Dir
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Errorf("failed to restore packages: %s\n%s", err, out)
+		} else {
+			t.Log(string(out))
+		}
+	}
+}
+
 type ConvertedProgram struct {
+	Language    string
 	Dir         string
 	UpdateSteps []UpdateStep
 }
@@ -75,5 +151,6 @@ func (pt *ProviderTest) ConvertProgram(t *testing.T, language string) (*Converte
 	return &ConvertedProgram{
 		Dir:         convertedDir,
 		UpdateSteps: convertedEditDirs,
+		Language:    language,
 	}, nil
 }
