bb23356709314a27b1f033487444c24f5ac8123d
Daniel Bradley <daniel@pulumi.com>
2023-12-14 10:55:35 +0000

    Add log as recording starts

--- upgrade.go
+++ upgrade.go
@@ -458,6 +458,7 @@ func (b *providerUpgradeBuilder) checkProviderUpgradePreviewOnly(t *testing.T) {
 
 	// Skip if state not yet created
 	if _, err := os.Stat(info.stateFile); os.IsNotExist(err) {
+		t.Logf("No pre-recorded state found for %s, recording baseline behaviour.", b.baselineVersion)
 		b.providerUpgradeRecordBaselines(t)
 	}
 
