ea4711afad754ce25deff27cd27791f67e05294b
Daniel Bradley <daniel@pulumi.com>
2024-01-04 14:49:54 +0000

    Add Provider Intercept Proxy (#48)
    * Fix temp dir early return
    
    Re-assign context and continue to next setup steps.
    
    * Add Provider Intercept Proxy
    
    Enable intercepting all gRPC interactions for any provider configured using attachment.

---
+++ providers/providerInterceptProxy_test.go
@@ -0,0 +1,30 @@
+package providers_test
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/pulumi/providertest/providers"
+	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/providertest/pulumitest/opttest"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func TestProviderInterceptProxy(t *testing.T) {
+	ctx := context.Background()
+	didAttach := false
+	gcpProviderFactory := providers.DownloadPluginBinaryFactory("azure-native", "2.21.0")
+	interceptedFactory := providers.ProviderInterceptFactory(ctx, gcpProviderFactory, providers.ProviderInterceptors{
+		Attach: func(ctx context.Context, in *pulumirpc.PluginAttach, client pulumirpc.ResourceProviderClient) (*emptypb.Empty, error) {
+			didAttach = true
+			return client.Attach(ctx, in)
+		},
+	})
+	test := pulumitest.NewPulumiTest(t, filepath.Join("..", "pulumitest", "testdata", "yaml_azure"), opttest.AttachProvider("azure-native", interceptedFactory))
+	test.SetConfig("azure-native:location", "WestUS2")
+	test.Preview()
+	assert.True(t, didAttach, "expected Attach to be called")
+}
