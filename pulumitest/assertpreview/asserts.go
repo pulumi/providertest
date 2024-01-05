3c9fa7eb7f658904819bfab5dc7d370888d9e085
Daniel Bradley <daniel@pulumi.com>
2024-01-05 14:53:27 +0000

    Add HasNoReplacements asserts

--- pulumitest/assertpreview/asserts.go
+++ pulumitest/assertpreview/asserts.go
@@ -29,3 +29,14 @@ func HasNoDeletes(t *testing.T, preview auto.PreviewResult) {
 		t.Errorf("expected no changes, got %s\n%s", unexpectedOps, preview.StdOut)
 	}
 }
+
+func HasNoReplacements(t *testing.T, preview auto.PreviewResult) {
+	t.Helper()
+
+	convertedMap := changesummary.ChangeSummary(preview.ChangeSummary)
+	unexpectedOps := convertedMap.WhereOpEquals(apitype.OpReplace, apitype.OpCreateReplacement, apitype.OpDeleteReplaced, apitype.OpDiscardReplaced, apitype.OpImportReplacement, apitype.OpReadReplacement)
+
+	if len(*unexpectedOps) > 0 {
+		t.Errorf("expected no replacements, got %s\n%s", unexpectedOps, preview.StdOut)
+	}
+}
