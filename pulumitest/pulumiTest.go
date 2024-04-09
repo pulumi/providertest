d80a08be10c4569faf5339422c355300cd68344f
Venelin <venelin@pulumi.com>
2024-04-09 15:35:07 +0100

    update interface

--- pulumitest/pulumiTest.go
+++ pulumitest/pulumiTest.go
@@ -2,7 +2,6 @@ package pulumitest
 
 import (
 	"context"
-	"testing"
 	"time"
 
 	"github.com/pulumi/providertest/pulumitest/opttest"
@@ -10,7 +9,24 @@ import (
 )
 
 type T interface {
-	testing.TB
+	Cleanup(func())
+	Error(args ...any)
+	Errorf(format string, args ...any)
+	Fail()
+	FailNow()
+	Failed() bool
+	Fatal(args ...any)
+	Fatalf(format string, args ...any)
+	Helper()
+	Log(args ...any)
+	Logf(format string, args ...any)
+	Name() string
+	Setenv(key, value string)
+	Skip(args ...any)
+	SkipNow()
+	Skipf(format string, args ...any)
+	Skipped() bool
+	TempDir() string
 	Deadline() (deadline time.Time, ok bool)
 }
 
