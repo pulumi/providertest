7387849e08c51448cb97f798e8725e49281d5204
VenelinMartinov <venelin.v.martinov@gmail.com>
2024-07-16 14:43:26 +0300

    Add import to pulumitest (#91)
    * add import to pulumitest
    
    * remove state function, add return vals
    
    * add import test
    
    * add stack state assertion

---
+++ pulumitest/import.go
@@ -0,0 +1,20 @@
+package pulumitest
+
+func (a *PulumiTest) Import(
+	resourceType, resourceName, resourceID string, providerUrn string,
+) cmdOutput {
+	a.t.Helper()
+	arguments := []string{
+		"import", resourceType, resourceName, resourceID, "--yes", "--protect=false", "-s", a.CurrentStack().Name(),
+	}
+	if providerUrn != "" {
+		arguments = append(arguments, "--provider="+providerUrn)
+	}
+	ret := a.execCmd(arguments...)
+	if ret.ReturnCode != 0 {
+		a.log(ret.Stdout)
+		a.fatalf("failed to import resource %s: %s", resourceName, ret.Stderr)
+	}
+
+	return ret
+}
---
+++ pulumitest/import_test.go
@@ -0,0 +1,40 @@
+package pulumitest_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/pulumi/providertest/pulumitest"
+	"github.com/stretchr/testify/require"
+)
+
+func TestImport(t *testing.T) {
+	t.Parallel()
+	test := pulumitest.NewPulumiTest(t, "testdata/yaml_empty")
+
+	res := test.Import("random:index/randomString:RandomString", "str", "importedString", "")
+
+	// Assert on the generated YAML containing a resource definition
+	require.Contains(t, res.Stdout, "type: random:RandomString")
+
+	// Assert on the stack containing the resource state
+	stack := test.ExportStack()
+	data, err := stack.Deployment.MarshalJSON()
+	require.NoError(t, err)
+	var stateMap map[string]interface{}
+	err = json.Unmarshal(data, &stateMap)
+	require.NoError(t, err)
+
+	resourcesJSON := stateMap["resources"].([]interface{})
+
+	for _, res := range resourcesJSON {
+		// get the id
+
+		id := res.(map[string]interface{})["id"]
+		if id == "importedString" {
+			return
+		}
+	}
+
+	t.Fatalf("resource not found in state")
+}
