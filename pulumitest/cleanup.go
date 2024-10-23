001f2457c129f9e052f266f18755094689220649
Daniel Bradley <daniel@pulumi.com>
2024-10-23 12:12:33 -0700

    Simplify env checks

--- pulumitest/cleanup.go
+++ pulumitest/cleanup.go
@@ -5,29 +5,23 @@ import (
 	"strings"
 )
 
-// We use this interesting contraption of immediately invoked callbacks to
-// eagerly calculate these variables but ensure they're not mutated.
-
-var skipDestroyOnFailure = (func() func() bool {
+func skipDestroyOnFailure() bool {
 	value, ok := os.LookupEnv("PULUMITEST_SKIP_DESTROY_ON_FAILURE")
 	skipDestroy := ok && strings.EqualFold(value, "true")
-	return func() bool { return skipDestroy }
-})()
+	return skipDestroy
+}
 
-var runningInCI = (func() func() bool {
+func runningInCI() bool {
 	_, ok := os.LookupEnv("CI")
-	return func() bool { return ok }
-})()
+	return ok
+}
 
-var shouldRetainFilesOnFailure = (func() func() bool {
+func shouldRetainFilesOnFailure() bool {
 	if value, ok := os.LookupEnv("PULUMITEST_RETAIN_FILES_ON_FAILURE"); ok {
-		if strings.EqualFold(value, "false") {
-			return func() bool { return false }
-		}
-		return func() bool { return true }
+		return !strings.EqualFold(value, "false")
 	}
 	if skipDestroyOnFailure() {
-		return func() bool { return true }
+		return true
 	}
-	return func() bool { return !runningInCI() }
-})()
+	return !runningInCI()
+}
