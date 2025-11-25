9a6039047cd64f9194ddbb780e37a6cde104ddb9
Alberto Pose <albertopose@gmail.com>
2025-11-25 13:47:45 +0000

    Add logging back

--- pulumitest/grpcLog_test.go
+++ pulumitest/grpcLog_test.go
@@ -6,12 +6,23 @@ import (
 	"github.com/gkampitakis/go-snaps/match"
 	"github.com/gkampitakis/go-snaps/snaps"
 	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGrpc(t *testing.T) {
 	t.Parallel()
 	test := pulumitest.NewPulumiTest(t, "testdata/yaml_program")
+
+	versionCmd := test.CurrentStack().Workspace().PulumiCommand()
+	version, _, _, err := versionCmd.Run(test.Context(), test.WorkingDir(), nil, nil, nil, nil, "version")
+	contract.AssertNoErrorf(err, "failed to get pulumi version: %s", version)
+	t.Logf("pulumi version: %s", version)
+
+	plugins, _, _, err := versionCmd.Run(test.Context(), test.WorkingDir(), nil, nil, nil, nil, "plugin", "ls", "-p")
+	contract.AssertNoErrorf(err, "failed to get pulumi plugins: %s", plugins)
+	t.Logf("pulumi plugins: %s", plugins)
+
 	test.Up(t)
 	log := test.GrpcLog(t)
 	assert.NotEmpty(t, log)
